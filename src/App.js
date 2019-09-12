9e78b177d436dc1cac58f0c51cfc251deeeb8136
vispaduchuri <vispaduchuri@gmail.com>
2019-09-12 12:26:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,11 +66,11 @@ class App extends Component {
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title">
-              BOARD LIST
+              BOOK LIST
             </h3>
           </div>
           <div class="panel-body">
-            <h4><Link to="/create">Add Board</Link></h4>
+            <h4><Link to="/create">Add Book</Link></h4>
             <table class="table table-stripe">
               <thead>
                 <tr>
